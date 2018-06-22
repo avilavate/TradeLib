04a6c2ce3d95a66ef1bba45bac0524af3a3c1bb7
A <avilavate@live.in>
2018-06-22 15:15:51 +0530

    Added test coverage to test profit validation.

--- TradePortal/TradePortal.Test/UnitTest1.cs
+++ TradePortal/TradePortal.Test/UnitTest1.cs
@@ -42,7 +42,31 @@ namespace TradePortal.Test
             tx.Buy();
             var walletBalanceAfter = WalletService.WalletBalance;
             float GSTRate = 5.0F;
-            Assert.IsTrue((walletBalanceBefore - walletBalanceAfter )== (3000 * 10) * (GSTRate / 100) + 3000 * 10);
+            Assert.IsTrue((walletBalanceBefore - walletBalanceAfter) == (3000 * 10) * (GSTRate / 100) + 3000 * 10);
+
+        }
+
+        [TestMethod]
+        public void Check_Profit_Validator()
+        {
+            //Buy some gold
+            WalletService.ResetWallet();
+            var tx = new Transaction(CommodityService.CommodityBalance, new Trade(3000, 10));
+            var walletBalanceBefore = WalletService.WalletBalance;
+            tx.Buy();
+            var walletBalanceAfter = WalletService.WalletBalance;
+            var noProfitException = "The trade is not profitable";
+            //try selling some gold at lower price
+            try
+            {
+                tx.Sell(new GoldCommodity(5, 3000));
+            }
+            catch (Exception e)
+            {
+                Assert.IsNotNull(e);
+                Assert.IsTrue(e.Message==noProfitException);
+            }
+           
 
         }
     }
