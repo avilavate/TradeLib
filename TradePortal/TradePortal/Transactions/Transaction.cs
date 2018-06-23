e180b0a8204381e88f67cc9469c8dfcc1edcf32c
A <avilavate@live.in>
2018-06-23 09:54:50 +0530

    Implemented sell tax (100*gold quantity).

--- TradePortal/TradePortal/Transactions/Transaction.cs
+++ TradePortal/TradePortal/Transactions/Transaction.cs
@@ -66,7 +66,7 @@ namespace TradePortal.Transactions
                 using (var ts = new TransactionScope())
                 {
                     TxCommodity.Quantity -= TxTrade.TradeQuantity;
-                    myWallet.Credit(TxTrade.GetTradeCost()-GoldTaxLoad.GoldBuyTransactionCharges);
+                    myWallet.Credit(TxTrade.GetTradeCost()-(GoldTaxLoad.GoldBuyTransactionCharges * sellingCommodity.Quantity));
                 }
             }
         }
